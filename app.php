206f0ba96cdb3e66735811d9a04534baa1e7f42f
danny aguilar <danny.aguilar@usach.cl>
2018-04-14 22:49:33 -0300

    new script

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-define('APPLICATION_NAME', 'Google Slides API PHP Quickstart');
+define('APPLICATION_NAME', 'script');
 define('CREDENTIALS_PATH', '~/.credentials/slides.googleapis.com-php-quickstart.json');
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 //var_dump(CLIENT_SECRET_PATH);
@@ -21,12 +21,6 @@ if (php_sapi_name() != 'cli') {
  * @return Google_Client the authorized client object
  */
 function getClient() {
-
-    // $client = new Google_Client();
-    // $client->setAuthConfig(CLIENT_SECRET_PATH);
-    // $client->setApplicationName(APPLICATION_NAME);
-    // $client->setScopes(SCOPES);
-    // return $client;
 
 
   $client = new Google_Client();
@@ -86,43 +80,6 @@ $service = new Google_Service_Slides($client);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$titulo = 'titulo';
-
-$presentation = new Google_Service_Slides_Presentation(array(
-  'title' => $titulo
-));
-
-//var_dump($service->presentations->create($presentation));
-
-$presentation = $service->presentations->create($presentation);
-printf("Created presentation with ID: %s\n", $presentation->presentationId);
-// Prints the number of slides and elements in a sample presentation:
-// https://docs.google.com/presentation/d/1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc/edit
-
-
-$presentationId = $presentation->presentationId;
-$presentation = $service->presentations->get($presentationId);
-$slides = $presentation->getSlides();
-
 // printf("The presentation contains %s slides:\n", count($slides));
 // foreach ($slides as $i => $slide) {
 //   // Print columns A and E, which correspond to indices 0 and 4.
@@ -130,20 +87,169 @@ $slides = $presentation->getSlides();
 //       count($slide->getPageElements()));
 // }
 
-$requests = [];
-$requests[] = new Google_Service_Slides_Request(array(
-  'createSlide' => array (
-    'objectId' => $pageId,
-    'insertionIndex' => count($slides),
-    'slideLayoutReference' => array (
-      'predefinedLayout' => 'TITLE_AND_BODY'
-    )
-  )
-));
 
-$batchUpdateRequest = new Google_Service_Slides_BatchUpdatePresentationRequest(array(
-  'requests' => $requests
-));
-$response = $service->presentations->batchUpdate($presentationId, $batchUpdateRequest);
-$createSlideResponse = $response->getReplies()[0]->getCreateSlide();
-printf("Created slide with ID: %s\n", $createSlideResponse->getObjectId());
+
+
+
+function getMainId($titulo,$service){
+  $title = $titulo;
+
+  $presentation = new Google_Service_Slides_Presentation(array(
+    'title' => $title,
+  ));
+
+  $presentation = $service->presentations->create($presentation);
+  printf("Created presentation with ID: %s\n", $presentation->presentationId);
+
+  $presentationId = $presentation->presentationId;
+  return $presentationId;
+}
+
+function init($presentationId,$service){
+  $presentation = $service->presentations->get($presentationId);
+  $slides = $presentation->getSlides();
+  return $slides;
+}
+
+
+function createSlide($layout,$slidesId,$slides,$presentationId,$service){
+  $requests = [];
+  if (!isset($layout)) {
+    $requests[] = new Google_Service_Slides_Request(array(
+      'createSlide' => array (
+        'insertionIndex' => count($slides),
+      )
+    ));
+  }
+  else {
+    $requests[] = new Google_Service_Slides_Request(array(
+      'createSlide' => array (
+        'insertionIndex' => count($slides),
+        'slideLayoutReference' => array (
+          'predefinedLayout' => 'TITLE_AND_BODY'
+        )
+      )
+    ));
+  }
+
+  $batchUpdateRequest = new Google_Service_Slides_BatchUpdatePresentationRequest(array(
+    'requests' => $requests
+  ));
+  $response = $service->presentations->batchUpdate($presentationId, $batchUpdateRequest);
+  $createSlideResponse = $response->getReplies()[0]->getCreateSlide();
+  //printf("Created slide with ID: %s\n", $createSlideResponse->getObjectId());
+  $ids = $slidesId;
+  array_push($ids,$createSlideResponse->getObjectId());
+  return $ids;
+}
+
+
+
+function getSlides($presentationId,$service){
+  $response = $service->presentations->get($presentationId);
+  $slides = $response->getSlides();
+  return $slides;
+}
+
+
+function insertText($text,$size,$objectId,$numberSlide,$presentationId,$service){
+  // Create a new square textbox, using the supplied element ID.
+  $height = array('magnitude' => $size[0], 'unit' => 'PT');
+  $width = array('magnitude' => $size[1], 'unit' => 'PT');
+  $requests = array();
+  $requests[] = new Google_Service_Slides_Request(array(
+    'createShape' => array (
+      'objectId' => $objectId,
+      'shapeType' => 'TEXT_BOX',
+      'elementProperties' => array(
+        'pageObjectId' => $numberSlide,
+        'size' => array(
+          'height' => $height,
+          'width' => $width
+        ),
+        'transform' => array(
+          'scaleX' => 1,
+          'scaleY' => 1,
+          'translateX' => $size[2],
+          'translateY' => $size[3],
+          'unit' => 'PT'
+        )
+      )
+    )
+  ));
+
+  // Insert text into the box, using the supplied element ID.
+  $requests[] = new Google_Service_Slides_Request(array(
+    'insertText' => array(
+      'objectId' => $objectId,
+      'insertionIndex' => 0,
+      'text' => $text
+    )
+  ));
+  // Execute the requests.
+  $batchUpdateRequest = new Google_Service_Slides_BatchUpdatePresentationRequest(array(
+    'requests' => $requests
+  ));
+  $response = $service->presentations->batchUpdate($presentationId, $batchUpdateRequest);
+  $createShapeResponse = $response->getReplies()[0]->getCreateShape();
+  printf("Created textbox with ID: %s\n", $createShapeResponse->getObjectId());
+}
+
+// function deleteSlide($id,$slidesId,$presentationId,$service){
+//   $requests = [];
+//   $requests[] = new Google_Service_Slides_Request(array(
+//     'deleteObject' => array (
+//       'objectid' => $id,
+//     )
+//   ));
+//
+//   $batchUpdateRequest = new Google_Service_Slides_BatchUpdatePresentationRequest(array(
+//     'requests' => $requests
+//   ));
+//   $response = $service->presentations->batchUpdate($presentationId, $batchUpdateRequest);
+//   $createSlideResponse = $response->getReplies()[0]->getCreateSlide();
+//
+//   $ids = $slidesId;
+//   $ids2 = [];
+//   foreach ($ids as $element) {
+//     if ($element != $id) {
+//       array_push($ids2,$createSlideResponse->getObjectId());
+//     }
+//   }
+//   return $ids2;
+// }
+
+// {
+//   altura
+//   anchura
+//   translate x
+//   translate y
+// }
+
+
+
+
+//entrada
+$texto = [
+  ['los patos','los patos no tienen eco'],
+  ['los gatos','los gatos son malevolos'],
+];
+
+$slidesId = [];
+$idIncremental = 0;
+
+$presentationId = getMainId('titulo',$service);
+$slides = init($presentationId,$service);
+
+
+foreach ($texto as $element) {
+  $slidesId = createSlide(null,$slidesId,$slides,$presentationId,$service);
+  insertText($element[0],[48,670,27,35],'text'.strval($idIncremental),$slidesId[count($slidesId)-1],$presentationId,$service);
+  $idIncremental++;
+  insertText($element[1],[250,670,27,95],'text'.strval($idIncremental),$slidesId[count($slidesId)-1],$presentationId,$service);
+  $idIncremental++;
+}
+//echo 'sdklgjhslkfjdgh';
+
+//var_dump(count($slides));
+$slides = getSlides($presentationId,$service);
